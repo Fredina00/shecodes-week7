99168368ffe36c91dfaeb41c9b56199ee9c49093
Fredina00 <152204723+Fredina00@users.noreply.github.com>
2024-01-31 21:38:46 +0000

    added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-city");
+
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "2ocb7acd896tf43b0e5f09484729f44d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
- let cityElement = document.querySelector("#weather-city");
- cityElement.innerHTML = searchInput.value;
+  
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Accra");
