f69d1bf0baba12e5c3b935da5574654a49cc823a
Fredina00 <152204723+Fredina00@users.noreply.github.com>
2024-02-01 20:50:52 +0000

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,40 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
+  speedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+}
+
+function formatDate(date) {
+  let minute = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Sunday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minute < 10) {
+    minutes = `0${minute}`;
+  }
+
+  return `${day} ${hours}:${minute}`;
 }
 
 function searchCity(city) {
